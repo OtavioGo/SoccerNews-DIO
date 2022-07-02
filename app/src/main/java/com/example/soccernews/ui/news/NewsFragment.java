e7093f3823954e6695705df1014b87d05d5c88e4
Otavio Gomes <otavio.gomestemporim@gmail.com>
2022-07-02 11:34:37 -0300

    Adicionando o Adapter para o Recycler View

--- app/src/main/java/com/example/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsFragment.java
@@ -9,23 +9,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.soccernews.databinding.FragmentNewsBinding;
+import com.example.soccernews.ui.adapters.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel =
-                new ViewModelProvider(this).get(NewsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-
+            binding.rvNews.setAdapter(new NewsAdapter(news));
         });
         return root;
     }
